a3fe26089f87fa0fcbc58f994d28dd4e329971fa
John Sauter <John_Sauter@systemeyescomputerstore.com>
2020-11-06 17:18:02 -0500

    Adjust future leap seconds starting in 2033.

--- build_RPMs.sh
+++ build_RPMs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# File: build_RPMs.sh, author: John Sauter, date: April 25, 2020.
+# File: build_RPMs.sh, author: John Sauter, date: November 6, 2020..
 # Build the RPMs for proleptic_UTC_with_leap_seconds.
 
 # Requires fedora-packager, rpmdevtools, copr-cli.
@@ -19,16 +19,25 @@ umask 022
 rm -f SOURCES/*
 rm -f SRPMS/*
 rm -f RPMS/noarch/*
-# Copy in the new tarball.
+# Copy in the new tarball and spec file.
 popd
 cp -v proleptic_utc_with_leap_seconds-*.tar.gz ~/rpmbuild/SOURCES/
+cp -v proleptic_utc_with_leap_seconds.spec ~/rpmbuild/SOURCES/
 pushd ~/rpmbuild/SOURCES
+# Set the file protections to the proper values.
 chmod 0644 proleptic_utc_with_leap_seconds-*.tar.gz
-# Build and test the source RPM.
-rpmbuild -ta proleptic_utc_with_leap_seconds-*.tar.gz
+chmod 0644 proleptic_utc_with_leap_seconds.spec
+# Build the source RPM.
+rpmbuild -bs proleptic_utc_with_leap_seconds.spec
 # Copy back the source RPM so it can be copied to github.
 popd
 cp -v ~/rpmbuild/SRPMS/proleptic_utc_with_leap_seconds-*.src.rpm .
+# Make sure it is OK.
+rpmlint proleptic_utc_with_leap_seconds-*.src.rpm
+# Further test the source RPM by building and testing the binary RPMs.
+pushd ~/rpmbuild/SOURCES
+rpmbuild -bb proleptic_utc_with_leap_seconds.spec
+popd
 # Perform validity checking on the RPMs.
 pushd ~/rpmbuild/SRPMS
 rpmlint proleptic_utc_with_leap_seconds-*.src.rpm
